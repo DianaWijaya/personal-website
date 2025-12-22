b0d241f19f6e07e3492ca1a5ee233e561500c2bb
DianaWijaya <72594154+DianaWijaya@users.noreply.github.com>
2025-12-22 11:59:15 +0700

    seprarate the data for education

---
+++ src/reusables/data/educationData.ts
@@ -0,0 +1,24 @@
+import { Education } from "../types";
+
+export const educationData: Education[] = [
+  {
+    id: 1,
+    institution: "Monash University",
+    degree: "Bachelor of Computer Science in Advanced Computer Science",
+    period: "July 2022 – June 2025",
+    location: "Malaysia",
+    gpa: "3.95/4.0",
+    wam: "87.59",
+    honors: "With Honors",
+  },
+  {
+    id: 2,
+    institution: "Sunway College",
+    degree: "Monash University Foundation Year (MUFY)",
+    period: "August 2021 – June 2022",
+    location: "Malaysia",
+    gpa: "4.0/4.0",
+    wam: "91.50",
+    honors: "With Honors",
+  }
+];
